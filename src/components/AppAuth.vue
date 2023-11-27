38dd1487cb8c8eca0ec613eaf2440f7e33479ffe
Ariel dos Santos Nunes <arielsn@outlook.com>
2023-11-27 19:38:38 -0300

    feat: created register function

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -23,6 +23,11 @@ export default {
         tos: "required"
       }
     }
+  },
+  methods: {
+    register(values, event) {
+      console.log(values)
+    }
   }
 }
 </script>
@@ -102,7 +107,7 @@ export default {
           </form>
 
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
