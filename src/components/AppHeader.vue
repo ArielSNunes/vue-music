da01c7b8059a91f0db33473b3628f68e494120d5
Ariel dos Santos Nunes <ariel.nunes@boxdelivery.com.br>
2023-12-06 21:03:39 -0300

    feat: created manage page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -29,13 +29,16 @@ export default {
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
+          <li>
+            <router-link class="px-2 text-white" to="/about">About</router-link>
+          </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">Login / Register</a>
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" to="/manage">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "@/views/HomeView.vue"
 import AboutView from "@/views/AboutView.vue"
+import ManageView from "@/views/ManageView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/about",
       component: AboutView,
       name: "about"
+    },
+    {
+      path: "/manage",
+      name: "manage",
+      component: ManageView
     }
   ]
 })
