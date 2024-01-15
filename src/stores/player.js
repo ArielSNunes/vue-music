009a5b8f92022d13840e6ad6c4bc39ed6a09b563
Ariel dos Santos Nunes <ariel.nunes@boxdelivery.com.br>
2024-01-15 11:27:47 -0300

    feat: updated audio player

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,12 +1,45 @@
 import { defineStore } from "pinia"
+import { Howl } from "howler"
 
 export const usePlayerStore = defineStore('player', {
     state: () => ({
-        song: {}
+        song: {},
+        sound: null
     }),
+    getters: {
+        hasPlayingSong: ({ sound }) => !!sound,
+        isPlaying: ({ sound }) => sound?.playing() ?? false
+    },
     actions: {
         async updateSong(song) {
             this.song = song
+            this.sound = new Howl({
+                src: [song.url],
+                html5: true
+            })
+
+            this.playSong()
+        },
+        playSong() {
+            if (!this.hasPlayingSong) {
+                return
+            }
+            this.sound.play()
+        },
+        stopSong() {
+            if (!this.hasPlayingSong) {
+                return
+            }
+            this.sound.pause()
+        },
+        toggleAudio() {
+            if (!this.hasPlayingSong) {
+                return
+            }
+            if (this.isPlaying) {
+                return this.stopSong()
+            }
+            return this.playSong()
         }
     }
 })
